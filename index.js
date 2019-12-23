9d882ba83bfd8dbedfc3595d16a2e6afab4ce51b
GraceeFlower <1491644103@qq.com>
2019-12-23 23:40:21 +0800

    [WangMin] feat: add the function to delete the items

--- index.js
+++ index.js
@@ -3,83 +3,118 @@ var todoList = document.getElementsByClassName("task-list")[0];
 var checkItem = document.getElementsByName("check-item");
 var sortList = document.getElementsByClassName("sort-list")[0];
 
-function loadItem() {
+function loadItem(status) {
   todoList.innerHTML = "";
   if(localStorage.length) {
-    for(var item = 0; item < localStorage.length; item++) {
-      addItem(item);
+    var localArr = [];
+    var doneArr = [];
+    var todoArr = [];
+    for(var item in localStorage) {
+      if(localStorage.hasOwnProperty(item)) {
+        var index = JSON.parse(localStorage[item])[0];
+        if(JSON.parse(localStorage[item])[3]) {
+          localArr[index] = JSON.parse(localStorage[item])[1];
+          if(JSON.parse(localStorage[item])[2]) {
+            doneArr[index] = JSON.parse(localStorage[item])[1];
+          } else {
+            todoArr[index] = JSON.parse(localStorage[item])[1];
+          }
+        } 
+      }
+    }
+    switch(status) {
+      case "todo":
+        todoArr.forEach((key) => addItem(key));
+        break;
+      case "done":
+        doneArr.forEach((key) => addItem(key));
+        break;
+      default:
+        localArr.forEach((key) => addItem(key));
+        break;
     }
   } 
 }
 
 function addStorage() {
-  if(itemValue.value) {
-    var localKey = localStorage.length;
-    var localValue = [itemValue.value, ""];
-    localStorage.setItem(localKey, JSON.stringify(localValue));
-    addItem(localKey);
-    itemValue.value = "";
-    itemValue.focus();
+  if (itemValue.value) {
+    var localKey = itemValue.value;
+    if (localStorage.getItem(localKey)) {
+      alert("You have created this item!");
+    } else {
+      var localValue = [localStorage.length, localKey, "", true];
+      localStorage.setItem(localKey, JSON.stringify(localValue));
+      addItem(localKey);
+      itemValue.value = "";
+      itemValue.focus();
+    }
   }
 }
 
 function addItem(key) {
   var value = JSON.parse(localStorage.getItem(key));
   var todoItem = document.createElement("li");
-  var checkState = value[1];
+  var checkState = value[2];
   todoItem.setAttribute("class", (checkState ? "done-item" : ""));
   todoItem.innerHTML = `
-    <input type="checkbox" name="check-item" ${checkState} />
-    <span>${value[0]}</span>
+    <input type="checkbox" name="check-item" ${checkState} /><span>${value[1]}</span>
+    <input type ="button" class="delete-btn" name="delete-item" value="✕"/>
   `
   todoList.appendChild(todoItem);
 }
 
-function getNodeIndex(list){
-  var index = 0;
-  while (list = list.previousSibling) {
-    index++;
-  }
-  return index;
+function changeState(list) {
+  var key = list.innerText;
+  var state = list.className ? "checked" : "";
+  var index = JSON.parse(localStorage.getItem(key))[0];
+  var localValue = [index, list.innerText, state, true];
+  localStorage.setItem(key, JSON.stringify(localValue));
 }
 
-function changeState(list) {
-  var key = getNodeIndex(list);
-  var state = list.className ? "checked" : "";
-  var localValue = [list.innerText, state];
-  localStorage.setItem(key, JSON.stringify(localValue));
+function changeStyle(target) {
+  var list = target.parentNode;
+  list.setAttribute("class", (list.className ? "" : "done-item"));
+  changeState(list);
+}
+
+function deleteItem(target) {
+  if (confirm("Are you sure to delete this item?")) {
+    var list = target.parentNode;
+    var key = list.innerText;
+    var value = JSON.parse(localStorage.getItem(key));
+    var index = value[0];
+    var state = value[2];
+    var localValue = [index, key, state, false];
+    localStorage.setItem(key, JSON.stringify(localValue));
+    loadItem();
+  }
 }
 
 todoList.addEventListener("click", function (event) {
   var target = event.target;
-  if(target.name === "check-item") {
-    var list = event.target.parentNode;
-    list.setAttribute("class", (list.className ? "" : "done-item"));
-    changeState(list);
+  switch(target.name) {
+    case ("check-item"):
+      changeStyle(target);
+      break;
+    case ("delete-item"):
+      deleteItem(target);
+      break;
+    default:
+      break;
   }
 })
-
-function reloadItem(state) {
-  todoList.innerHTML = "";
-  for(var item = 0; item < localStorage.length; item++) {
-    var values = JSON.parse(localStorage.getItem(item));
-    if(Boolean(values[1]) == state) {
-      addItem(item);
-    }
-  }
-}
 
 sortList.addEventListener("click", function (event) {
   var target = event.target;
   switch(target.name) {
     case ("choose-todo"):
-      reloadItem(false);
+      loadItem("todo");
       break;
     case ("choose-done"):
-      reloadItem(true);
+      loadItem("done");
       break;
     case ("choose-all"):
-      loadItem();
+      loadItem("all");
     default:
       break;
   }
