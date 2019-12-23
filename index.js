bb28beb11830f02caf4fa4d7b7a588cfd01cd6a4
GraceeFlower <1491644103@qq.com>
2019-12-23 11:13:48 +0800

    [WangMin] feat: create the function about loading & adding todo items

---
+++ index.js
@@ -0,0 +1,31 @@
+var list = document.getElementById("todo-list");
+var itemValue = document.getElementsByName("item-value")[0];
+var todoList = document.getElementsByClassName("task-list")[0];
+var localCount = 0;
+function addStorage() {
+  var localKey = localCount++;
+  var localValue = itemValue.value;
+  localStorage.setItem(localKey, localValue);
+  addItem(localValue);
+  itemValue.value = "";
+  itemValue.focus();
+}
+
+function loadItem() {
+  for(var item = 0; item < localStorage.length; item++) {
+    var values = localStorage.getItem(item);
+    addItem(values);
+  }
+}
+
+function addItem(value) {
+  var todoItem = document.createElement("li");
+  todoItem.innerHTML = `
+    <input type="checkbox" name="check-item" />
+    <span>${value}</span>
+  `
+  todoList.appendChild(todoItem);
+}
+
+loadItem();
+
